c2a697e9a801d02229a03bfd3b93ddf8a7a2c749
Swain, Susrut (Cognizant) <Susrut.Swain@otiselevator.net>
2025-01-02 16:40:53 +0530

    Initial Commit

---
+++ LittleLemonMenu/LittleLemonMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonMenuApp.swift
+//  LittleLemonMenu
+//
+//  Created by Swain, Susrut (Cognizant) on 02/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
